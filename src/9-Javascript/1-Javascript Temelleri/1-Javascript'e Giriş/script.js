7e3fa55743b937d7caa3bd92997525f380a54ee8
ahmet-bedir41 <72944816+ahmet-bedir41@users.noreply.github.com>
2020-10-18 22:59:37 +0300

    Update script.js

--- src/9-Javascript/1-Javascript Temelleri/1-Javascript'e Giriş/script.js
+++ src/9-Javascript/1-Javascript Temelleri/1-Javascript'e Giriş/script.js
@@ -1,9 +1,7 @@
 console.log('Merhaba')
 console.log(1234)
 console.log(true)
-console.log([1,2,3])
-console.error('Hata oluştu')
-console.warn('Bir uyarı oluştu')
+console.log([1,2,3]) arı oluştu')
 
 // tek satırlık yorum satırı
 
